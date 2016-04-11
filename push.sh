abcf21bcc9d4f653098181a86e8fab95714a97a8
brianbenns <bbe@trustpilot.com>
2016-04-10 20:16:40 -0400

    added timeline-no-heart

--- push.sh
+++ push.sh
@@ -4,6 +4,6 @@ docker build -t bbenns/wedding-website:latest .
 docker push bbenns/wedding-website:latest
 aws ecs register-task-definition --cli-input-json file://task-def.json
 aws ecs update-service --cluster websites --service wedding-website --desired-count 0
-sleep 25
+sleep 35
 aws ecs update-service --cluster websites --service wedding-website --task-definition wedding-website
 aws ecs update-service --cluster websites --service wedding-website --desired-count 1
