a8d24b21510208b6f05ab861b20d4cb16d7e58ba
brianbenns <bbe@trustpilot.com>
2016-05-18 21:51:07 -0400

    mobile fixes

--- push.sh
+++ push.sh
@@ -2,8 +2,8 @@
 
 docker build -t bbenns/wedding-website:latest .
 docker push bbenns/wedding-website:latest
-aws ecs register-task-definition --cli-input-json file://task-def.json
-aws ecs update-service --cluster websites --service wedding-website --desired-count 0
+aws ecs register-task-definition --cli-input-json file://task-def.json --profile personal
+aws ecs update-service --cluster websites --service wedding-website --desired-count 0  --profile personal
 sleep 35
-aws ecs update-service --cluster websites --service wedding-website --task-definition wedding-website
-aws ecs update-service --cluster websites --service wedding-website --desired-count 1
+aws ecs update-service --cluster websites --service wedding-website --task-definition wedding-website --profile personal
+aws ecs update-service --cluster websites --service wedding-website --desired-count 1 --profile personal
