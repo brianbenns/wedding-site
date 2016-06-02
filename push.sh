34326b712ef0ee77122e32c2a5a210688fa10fb0
Ubuntu <ubuntu@ip-172-30-0-94.ec2.internal>
2016-06-02 01:18:07 +0000

    file name

--- push.sh
+++ push.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-
-docker build -t bbenns/wedding-website:latest .
-docker push bbenns/wedding-website:latest
+sudo docker build -t bbenns/wedding-website:latest .
+sudo docker push bbenns/wedding-website:latest
 aws ecs register-task-definition --cli-input-json file://task-def.json --profile personal
 aws ecs update-service --cluster websites --service wedding-website --desired-count 0  --profile personal
 sleep 35
